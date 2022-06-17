7b2cbe18592f1afeaee068d8e35c168e801d6d3d
ciorca <ciorca.gaby@gmail.com>
2022-06-17 17:17:27 +0300

    Add project files.

---
+++ WritingLINQQueries/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WritingLINQQueries
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IEnumerable<Student> queryResult = from student in Student.GetAllStudents()
+
+                                               where student.Gender == "Male"
+                                               select student;
+            GridView1.DataSource = queryResult;
+            GridView1.DataBind();
+        }
+    }
+}
